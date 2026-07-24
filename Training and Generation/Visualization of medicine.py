0543160c8d62f1a2430617df06506710f0fd26ef
myw35586 <129239080+myw35586@users.noreply.github.com>
2026-07-24 15:51:17 +0800

    Update and rename 成药可视化.py to Visualization of medicine.py

--- Training and Generation/Visualization of medicine.py
+++ Training and Generation/Visualization of medicine.py
@@ -3,11 +3,11 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 from rdkit.Chem import AllChem
 
-# 1. 读取你刚刚生成的 CSV
+
 csv_path = "/home/myw/drugvae/3D_model/data/multi_point_results.csv"
 df = pd.read_csv(csv_path)
 
-# 2. 取前 9 个最好的分子
+
 top_mols_df = df.head(9)
 
 mols = []
@@ -22,10 +22,10 @@ for index, row in top_mols_df.iterrows():
     AllChem.Compute2DCoords(mol)
     mols.append(mol)
     
-    # 图片下方的文字说明
+
     legends.append(f"Rank {index+1}\nQED: {qed:.3f}")
 
-# 3. 画网格图
+
 img = Draw.MolsToGridImage(
     mols, 
     molsPerRow=3, 
